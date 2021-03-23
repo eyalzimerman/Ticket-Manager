34b2a7ab4693bacfa1c48e87ca874f911a9514a7
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-23 15:00:08 +0200

    added ticket schema

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Tickets = require("./model/tickets");
 
 app.use(express.static("client/build"));
+app.use(express.json());
 
 module.exports = app;
