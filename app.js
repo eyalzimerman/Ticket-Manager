fc3b00af2251a63893da9a843c303c5c63f4d364
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-23 15:00:50 +0200

    added GET route

--- app.js
+++ app.js
@@ -6,4 +6,21 @@ const Tickets = require("./model/tickets");
 app.use(express.static("client/build"));
 app.use(express.json());
 
+app.get("/api/tickets", async (req, res) => {
+  try {
+    let data = await Tickets.find({});
+    const { searchText } = req.query;
+    if (searchText) {
+      data = data.filter((search) => {
+        return search.title.toLowerCase().includes(searchText.toLowerCase());
+      });
+    }
+
+    res.status(200).send(data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Server Problem");
+  }
+});
+
 module.exports = app;
