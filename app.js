bc105d022954c3c37b4bfef0825e8c3c0985596a
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-23 15:04:25 +0200

    added PATCH route for undone

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Tickets = require("./model/tickets");
 app.use(express.static("client/build"));
 app.use(express.json());
 
+// GET all tickets or by searching title text
 app.get("/api/tickets", async (req, res) => {
   try {
     let data = await Tickets.find({});
@@ -23,10 +24,28 @@ app.get("/api/tickets", async (req, res) => {
   }
 });
 
+// PATCH ticket to done by ticket id and /done route
 app.patch("/api/tickets/:ticketId/done", (req, res) => {
   const { ticketId } = req.params;
 
   Tickets.findByIdAndUpdate(ticketId, { done: true }, { new: true })
+    .then(() => {
+      res.status(200).send({ updated: true });
+    })
+    .catch((e) => {
+      if (e.name === "CastError") {
+        res.status(404).send({ message: e.name, updated: false });
+      } else {
+        res.status(500).send({ message: "MongooseError", updated: false });
+      }
+    });
+});
+
+// PATCH ticket to undone by ticket id and /undone route
+app.patch("/api/tickets/:ticketId/undone", (req, res) => {
+  const { ticketId } = req.params;
+
+  Tickets.findByIdAndUpdate(ticketId, { done: false }, { new: true })
     .then(() => {
       res.status(200).send({ updated: true });
     })
